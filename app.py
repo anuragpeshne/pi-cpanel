7370e30742705761842655cc09872f2c154cd797
anuragpeshne <anurag.peshne@gmail.com>
2018-07-22 19:52:36 -0700

    shutdown working

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import subprocess
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    name = subprocess.check_output("hostname").decode("utf-8").strip()
     return render_template('index.html', name=name)
+
+@app.route("/cmd", methods=['GET'])
+def exec_cmd():
+    command = request.args["command"]
+    implemented_commands = {
+        "shutdown" : ["sudo", "shutdown", "-h", "now"],
+        "reboot"   : ["sudo", "shutdown", "-r", "now"],
+        "uptime"   : ["uptime"]}
+    if command:
+        if command in implemented_commands:
+            output = subprocess.check_output(implemented_commands[command])
+        else:
+            output = "Unimplemented/Invalid Command"
+    else:
+        output = "Empty Command"
+    return output
 
 if __name__ == "__main__":
     app.run()
