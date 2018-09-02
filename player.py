f341646bd88357f64716b619c70a4207a3382537
Anurag Peshne <anurag.peshne@gmail.com>
2018-09-01 22:56:23 -0400

    moved to p-expect from subprocess

--- player.py
+++ player.py
@@ -1,54 +1,44 @@
-import subprocess
+import pexpect
 
 class Player:
     def __init__(self):
         self.reset()
 
     def decode_url(self, url):
-        ydl_proc = subprocess.Popen(["youtube-dl",
-                                     "--get-url", url,
-                                     "--playlist-start", str(self.playlist_index),
-                                     "--no-playlist"],
-                                    stdout = subprocess.PIPE,
-                                    stdin = subprocess.PIPE)
-        try:
-            outs, errs = ydl_proc.communicate(input=None,timeout=15)
-            outs = outs.decode("utf-8").strip().split("\n")
-            self.playlist = outs
-        except:
-            outs = "Timeout during decoding"
-        return outs
+        ydl_proc = pexpect.spawn('youtube-dl --get-url ' + url +
+                                 ' --playlist-start ' + str(self.playlist_index) +
+                                 ' --no-playlist')
+        ydl_proc.expect('http.*', timeout=15)
+        output = ydl_proc.after.decode("utf-8")
+        links = output.strip().split('\n')
+        self.playlist = links
+        return links
 
     def play(self):
-        self.omx_proc = subprocess.Popen(["omxplayer",
-                                          self.playlist[self.playlist_index]],
-                                         stdout = subprocess.PIPE,
-                                         stdin = subprocess.PIPE)
-        try:
-            outs, errs = self.omx_proc.communicate(input=None,timeout=3)
-        except:
-            outs = outs
-            pass
-        return outs.decode("utf-8")
+        self.omx_proc = pexpect.spawn('omxplayer ' + self.playlist[self.playlist_index - 1])
+        self.omx_proc.expect("Video.*")
+        return self.omx_proc.after.decode("utf-8")
 
     def control_omx_proc(self, req):
         if (req == "vol_up"):
             cmd = "+"
+            exp = "Current Volume: .*"
         elif (req == "vol_down"):
             cmd = "-"
+            exp = "Current Volume: .*"
         elif (req == "pause_toggle"):
             cmd = "p"
+            exp = ".*"
         elif (req == "quit"):
             cmd = "q"
+            exp = "Stopped at.*"
 
-        try:
-            outs, errs = self.omx_proc.communicate(input=cmd,timeout=1)
-        except:
-            outs = outs
-            pass
+        self.omx_proc.send(cmd)
+        self.omx_proc.expect(exp)
+        output = self.omx_proc.after.decode("utf-8")
         if (cmd == "q"):
             self.reset()
-        return outs.decode("utf-8")
+        return output
 
     def reset(self):
         self.playlist_index = 1
